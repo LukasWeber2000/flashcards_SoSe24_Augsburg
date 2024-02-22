3fefb05bd935e40af3d8e77b7431f6e7e815ba99
Manuel Buerger <manuel.buerger@hs-augsburg.de>
2024-02-22 14:54:00 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:easy_flashcard/flashcard_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+
+import 'flashcard.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Flashcard> _flashcards = [
+    Flashcard(
+        question: 'What programming language does Flutter use?',
+        answer: 'Dart'),
+    Flashcard(question: 'Who is the best programmer?', answer: 'Manu Klima'),
+    Flashcard(question: 'Whats two plus two', answer: '4 quick maths')
+  ];
+
+  final _imagePath = 'images/FlipDeck_Lettering.png';
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor:
+            Colors.white10, // Set the background color here
+      ),
+      home: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            flexibleSpace: Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceBetween, // Align items to start and end of row
+              children: [
+                IconButton(
+                  onPressed: () {
+                    // Implement your back button functionality here
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Color(0xFF549186),
+                  ),
+                ),
+                Image.asset(
+                  _imagePath,
+                  fit: BoxFit
+                      .contain, // Adjust the image's fit within the app bar
+                  height: 40, // Adjust the height of the image as needed
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Implement your back button functionality here
+                  },
+                  icon: Icon(
+                    Icons.menu,
+                    color: Color(0xFF549186),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 400,
+                  height: 250,
+                  child: FlipCard(
+                      front: FlashcardView(
+                        text: _flashcards[_currentIndex].question,
+                      ),
+                      back: FlashcardView(
+                        text: _flashcards[_currentIndex].answer,
+                      )),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: showPreviousCard,
+                      icon: Icon(Icons.chevron_left),
+                      label: Text('Prev'),
+                      style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.white),
+                    ),
+                    OutlinedButton.icon(
+                        onPressed: showNextCard,
+                        icon: Icon(Icons.chevron_right),
+                        label: Text('Next'),
+                        style: OutlinedButton.styleFrom(
+                            foregroundColor: Colors.white))
+                  ],
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    _buildButton('Again', Colors.red),
+                    _buildButton('Difficult', Colors.orange),
+                    _buildButton('Good', Colors.yellow),
+                    _buildButton('Easy', Colors.green),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(String label, Color color) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        side: BorderSide(color: color),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: color), // Outline color
+          borderRadius: BorderRadius.circular(8), // Button border radius
+        ),
+      ),
+      child: Text(label),
+    );
+  }
+
+  void showNextCard() {
+    setState(() {
+      if (_currentIndex + 1 < _flashcards.length) {
+        _currentIndex++;
+      } else {
+        _currentIndex = 0;
+      }
+    });
+  }
+
+  void showPreviousCard() {
+    setState(() {
+      if (_currentIndex > 0) {
+        _currentIndex--;
+      } else {
+        _currentIndex = _flashcards.length - 1;
+      }
+    });
+  }
+}
