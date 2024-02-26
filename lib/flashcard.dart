1e97e144d84a6d88d7197c020e0013c1dbee2839
Manuel Buerger <manuel.buerger@hs-augsburg.de>
2024-02-26 17:05:51 +0100

    saving flashcards persistant

--- lib/flashcard.dart
+++ lib/flashcard.dart
@@ -1,6 +1,25 @@
 class Flashcard{
   final String question;
   final String answer;
+  String? hint;
 
-  Flashcard({ required this.question, required this.answer});
+  Flashcard({ required this.question, required this.answer, this.hint});
+
+  // Method to convert Flashcard object to a JSON object
+  Map<String, dynamic> toJson() {
+    return {
+      'question': question,
+      'answer': answer,
+      'hint': hint,
+    };
+  }
+
+  // Method to create Flashcard object from a JSON object
+  factory Flashcard.fromJson(Map<String, dynamic> json) {
+    return Flashcard(
+      question: json['question'],
+      answer: json['answer'],
+      hint: json['hint'],
+    );
+  }
 }
--- lib/flashcard_editor_view.dart
+++ lib/flashcard_editor_view.dart
@@ -1,10 +1,19 @@
 import 'package:easy_flashcard/deck.dart';
+import 'package:easy_flashcard/flashcard.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
 
 import 'main.dart';
 
 class FlashcardEditorView extends StatelessWidget {
   FlashcardEditorView({super.key});
+
+  final questionTextController = TextEditingController();
+  final answerTextController = TextEditingController();
+  final hintTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +48,7 @@ class FlashcardEditorView extends StatelessWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: doStuff,
                     icon: const Icon(
                       Icons.check,
                       color: Color(0xFF549186),
@@ -54,7 +63,7 @@ class FlashcardEditorView extends StatelessWidget {
                 Container(
                   decoration: const BoxDecoration(
                       border: Border(
-                      bottom: BorderSide(color: Colors.white),
+                    bottom: BorderSide(color: Colors.white),
                   )),
                   child: Padding(
                     padding: const EdgeInsets.only(left: 15.0, right: 15.0),
@@ -75,9 +84,8 @@ class FlashcardEditorView extends StatelessWidget {
                                 alignment: Alignment.center,
                                 value: decks.name,
                                 child: Text(decks.name,
-                                      style:
-                                          const TextStyle(color:  Color(0xFF549186))),
-
+                                    style: const TextStyle(
+                                        color: Color(0xFF549186))),
                               );
                             }).toList(),
 
@@ -85,6 +93,7 @@ class FlashcardEditorView extends StatelessWidget {
                             onChanged: (String? newValue) {
                               // You can put your logic here to respond to the selection of a new item
                               print('Selected item: $newValue');
+                              print(_localPath);
                             },
                           ),
                         ),
@@ -98,7 +107,7 @@ class FlashcardEditorView extends StatelessWidget {
                       border: Border(
                     bottom: BorderSide(color: Colors.white),
                   )),
-                  child: const Padding(
+                  child: Padding(
                     padding: EdgeInsets.all(8.0),
                     child: Row(
                       children: [
@@ -107,7 +116,7 @@ class FlashcardEditorView extends StatelessWidget {
                     ),
                   ),
                 ),
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 12.0),
                   child: SizedBox(
                     width: double.maxFinite,
@@ -125,6 +134,7 @@ class FlashcardEditorView extends StatelessWidget {
                         labelStyle: TextStyle(color: Color(0xFF549186)),
                         labelText: 'Hint',
                       ),
+                      controller: hintTextController,
                     ),
                   ),
                 ),
@@ -144,7 +154,7 @@ class FlashcardEditorView extends StatelessWidget {
                     ),
                   ),
                 ),
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 12.0),
                   child: SizedBox(
                     width: double.maxFinite,
@@ -162,6 +172,7 @@ class FlashcardEditorView extends StatelessWidget {
                         labelStyle: TextStyle(color: Color(0xFF549186)),
                         labelText: 'Question',
                       ),
+                      controller: questionTextController,
                     ),
                   ),
                 ),
@@ -181,7 +192,7 @@ class FlashcardEditorView extends StatelessWidget {
                     ),
                   ),
                 ),
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(top: 12.0),
                   child: SizedBox(
                     width: double.maxFinite,
@@ -199,6 +210,7 @@ class FlashcardEditorView extends StatelessWidget {
                         labelStyle: TextStyle(color: Color(0xFF549186)),
                         labelText: 'Answer',
                       ),
+                      controller: answerTextController,
                     ),
                   ),
                 ),
@@ -215,4 +227,65 @@ class FlashcardEditorView extends StatelessWidget {
     Deck(name: 'English'),
     Deck(name: 'Ein etwas längeres Kartendeck')
   ];
+
+
+  List<Flashcard> flashcards = [];
+
+  void doStuff() async {
+    flashcards.add(Flashcard(
+        question: questionTextController.text,
+        answer: answerTextController.text,
+        hint: hintTextController.text));
+
+    // Write the updated list of flashcards to the file
+    await writeFlashcardList(flashcards);
+
+    // Read all flashcards from the file
+    await readFlashcardList();
+  }
+
+  void readFlashcardListLocal(List<Flashcard> list) {
+    for (int i = 0; i < list.length; i++) {
+      print('Question: ' + list[i].question
+          + ' - Answer: ' + list[i].answer
+      + '- Hint:'+(list[i].hint??''));
+    }
+  }
+
+  Future<void> writeFlashcardList(List<Flashcard> flashcards) async {
+    final file = await _localFile;
+    final jsonList = flashcards.map((flashcard) => flashcard.toJson()).toList();
+    final jsonString = json.encode(jsonList);
+    await file.writeAsString(jsonString);
+  }
+
+  Future<void> readFlashcardList() async {
+    final file = await _localFile;
+    if (!await file.exists()) {
+      print('File does not exist');
+      return;
+    }
+
+    final jsonString = await file.readAsString();
+    final jsonList = json.decode(jsonString) as List<dynamic>;
+    flashcards = jsonList.map((json) => Flashcard.fromJson(json)).toList();
+
+    // Print all flashcards
+    for (int i = 0; i < flashcards.length; i++) {
+      print('Question: ${flashcards[i].question} - Answer: ${flashcards[i].answer} - Hint: ${flashcards[i].hint ?? ''}');
+    }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    print(path);
+    return File('$path/flashcards.json');
+  }
+
+
 }
